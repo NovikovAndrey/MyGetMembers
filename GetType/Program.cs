eb1957ff4aeff6f56a2b8240099c0027c7a123e6
Андрей Новиков <novikov.andrey.v@hotmail.com>
2020-02-12 23:28:50 +0300

    Get Constructors

--- GetType/Program.cs
+++ GetType/Program.cs
@@ -9,30 +9,45 @@ namespace GetType
         {
             
             Type myType = Type.GetType("GetType.User", false, true);
-            Console.WriteLine("Methods");
-            foreach(MethodInfo mi in myType.GetMethods())
+            Console.WriteLine("Constr");
+            foreach (ConstructorInfo constructorInfo in myType.GetConstructors())
             {
-                string modificator = "";
-                if (mi.IsStatic)
-                {
-                    modificator += "static";
-                }
-                if (mi.IsVirtual)
-                {
-                    modificator += "virtual";
-                }
-                Console.Write($"{modificator} {mi.ReturnType.Name} {mi.Name} (");
-                ParameterInfo[] parameterInfos = mi.GetParameters();
+                Console.Write(myType.Name +" (");
+                ParameterInfo[] parameterInfos = constructorInfo.GetParameters();
                 for (int i=0; i<parameterInfos.Length; i++)
                 {
-                    Console.Write($"{parameterInfos[i].ParameterType.Name} {parameterInfos[i].Name}");
-                    if (i+1< parameterInfos.Length)
+                    Console.Write(parameterInfos[i].ParameterType.Name + " "+parameterInfos[i].Name);
+                    if (i+1<parameterInfos.Length)
                     {
                         Console.Write(", ");
                     }
                 }
                 Console.WriteLine(")");
             }
+            //Console.WriteLine("Methods");
+            //foreach(MethodInfo mi in myType.GetMethods())
+            //{
+            //    string modificator = "";
+            //    if (mi.IsStatic)
+            //    {
+            //        modificator += "static";
+            //    }
+            //    if (mi.IsVirtual)
+            //    {
+            //        modificator += "virtual";
+            //    }
+            //    Console.Write($"{modificator} {mi.ReturnType.Name} {mi.Name} (");
+            //    ParameterInfo[] parameterInfos = mi.GetParameters();
+            //    for (int i=0; i<parameterInfos.Length; i++)
+            //    {
+            //        Console.Write($"{parameterInfos[i].ParameterType.Name} {parameterInfos[i].Name}");
+            //        if (i+1< parameterInfos.Length)
+            //        {
+            //            Console.Write(", ");
+            //        }
+            //    }
+            //    Console.WriteLine(")");
+            //}
         }
     }
 }
