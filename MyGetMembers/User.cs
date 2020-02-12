907da55d5233f44d3de6d95773b595bcba2848bf
Андрей Новиков <novikov.andrey.v@hotmail.com>
2020-02-12 21:34:38 +0300

    End

--- MyGetMembers/User.cs
+++ MyGetMembers/User.cs
@@ -6,5 +6,20 @@ namespace MyGetMembers
 {
     class User
     {
+        public string Name { get; set; }
+        public int Age { get; set; }
+        public User(string n, int a)
+        {
+            Name = n;
+            Age = a;
+        }
+        public void Display()
+        {
+            Console.WriteLine($"Name = {Name}, Age = {Age}");
+        }
+        public int Payment(int hours, int perHour)
+        {
+            return hours * perHour;
+        }
     }
 }
