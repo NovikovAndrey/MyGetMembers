b8360a22ff68f8c5fda7df36df436b11574ca301
Андрей Новиков <novikov.andrey.v@hotmail.com>
2020-02-12 23:31:57 +0300

    Add Properties and Fields

--- GetType/Program.cs
+++ GetType/Program.cs
@@ -9,21 +9,31 @@ namespace GetType
         {
             
             Type myType = Type.GetType("GetType.User", false, true);
-            Console.WriteLine("Constr");
-            foreach (ConstructorInfo constructorInfo in myType.GetConstructors())
+            Console.WriteLine("Fields");
+            foreach (FieldInfo fieldInfo in myType.GetFields())
             {
-                Console.Write(myType.Name +" (");
-                ParameterInfo[] parameterInfos = constructorInfo.GetParameters();
-                for (int i=0; i<parameterInfos.Length; i++)
-                {
-                    Console.Write(parameterInfos[i].ParameterType.Name + " "+parameterInfos[i].Name);
-                    if (i+1<parameterInfos.Length)
-                    {
-                        Console.Write(", ");
-                    }
-                }
-                Console.WriteLine(")");
+                Console.WriteLine($"{fieldInfo.FieldType} {fieldInfo.Name}");
             }
+            Console.WriteLine("Properties");
+            foreach(PropertyInfo propertyInfo in myType.GetProperties())
+            {
+                Console.WriteLine($"{propertyInfo.PropertyType} {propertyInfo.Name}");
+            }
+            //Console.WriteLine("Constr");
+            //foreach (ConstructorInfo constructorInfo in myType.GetConstructors())
+            //{
+            //    Console.Write(myType.Name +" (");
+            //    ParameterInfo[] parameterInfos = constructorInfo.GetParameters();
+            //    for (int i=0; i<parameterInfos.Length; i++)
+            //    {
+            //        Console.Write(parameterInfos[i].ParameterType.Name + " "+parameterInfos[i].Name);
+            //        if (i+1<parameterInfos.Length)
+            //        {
+            //            Console.Write(", ");
+            //        }
+            //    }
+            //    Console.WriteLine(")");
+            //}
             //Console.WriteLine("Methods");
             //foreach(MethodInfo mi in myType.GetMethods())
             //{
